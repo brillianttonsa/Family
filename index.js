daa72adbdf4ecb3b926d862fb63f70a6638ca127
brillianttonsa <abdullatifmnyamis@gmail.com>
2025-02-25 20:49:16 +0300

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const port = 7000;
 
+app.set("view engine", "ejs"); 
 app.use(express.static('public'));
 app.get('/', (req,res) => {
     res.render('index.html',)
